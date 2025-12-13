cdc7d969e4fd33038e7409b17760219239ea4003
graciapardede <graciapardede30@gmail.com>
2025-12-13 14:20:28 +0700

    newest

--- routes/auth.php
+++ routes/auth.php
@@ -30,10 +30,14 @@ Route::middleware('guest')->group(function () {
     Route::post('forgot-password/send', [ForgotPasswordController::class, 'sendReset'])
         ->name('password.send-reset');
 
-    Route::get('verify-reset-code', [ForgotPasswordController::class, 'showVerifyCode'])
-        ->name('password.verify-code');
+    Route::get('/verify-reset-code', [ForgotPasswordController::class, 'showVerifyCode'])
+        ->name('password.verify-code.form');
 
+<<<<<<< Updated upstream
     Route::post('verify-reset-code', [ForgotPasswordController::class, 'verifyCode'])
+=======
+    Route::post('/verify-reset-code', [ForgotPasswordController::class, 'verifyCode'])
+>>>>>>> Stashed changes
         ->name('password.verify-code');
 
     Route::get('reset-password-form', [ForgotPasswordController::class, 'showResetForm'])
