5f4a0a5e99d7ea3cbf99e54f28acb35714ddf755
graciapardede <graciapardede30@gmail.com>
2025-12-14 15:20:48 +0700

    prifilee

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->command->info('Starting Master Seeder...');
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table('redemption_items')->truncate();
         DB::table('redemptions')->truncate();
         DB::table('deposit_items')->truncate();
